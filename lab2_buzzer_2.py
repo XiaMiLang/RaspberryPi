f09a645c163a6e845533fd4b8bc4099759d401d3
XiaMiLang <n97071159@gs.ncku.edu.tw>
2019-10-10 04:05:38 +0800

    20191010

--- lab2_buzzer_2.py
+++ lab2_buzzer_2.py
@@ -10,6 +10,25 @@ def play_tunes(p, f, delay):
     p.ChangeFrequency(f)
     time.sleep(delay)
 
+def getFreqs(freq_num):
+    cand_freqs = [
+        261, # Do
+        277, # Do#
+        293, # Re#
+        311, # Re#
+        329, # Mi
+        349, # Fa
+        369, # Fa#
+        392, # Sol
+        415, # Sol#
+        440, # La
+        466, # La#
+        493, # Si
+        523  # Do
+    ]
+
+
+
 GPIO.setmode(GPIO.BOARD)	
 
 pin = int(sys.argv[1])		#read pin and tune from command line
@@ -19,9 +38,10 @@ p = GPIO.PWM(pin, 50)		#p = GPIO.PWM(channel, frequency)
 p.start(50)    #0 ~ 100
 delay = 0.2
 
-tunes = sys.argv[2].split(":")
 freq = []
-for fr in tunes:
+freqs = getFreqs(Freq_num)
+
+for fr in freqs:
     freq.append(int(fr))
 
 for f in freq:
