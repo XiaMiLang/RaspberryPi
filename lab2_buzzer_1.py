3814245dc4738c86207f564fdffcf67eabaa028b
XiaMiLang <n97071159@gs.ncku.edu.tw>
2019-10-10 03:15:28 +0800

    20191010

--- lab2_buzzer_1.py
+++ lab2_buzzer_1.py
@@ -1,31 +1,6 @@
 import time
 import RPi.GPIO as GPIO
 import sys
-"""
-def doReMi():
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(12, GPIO.OUT)
-    p = GPIO.PWM(12, 50)
-    p.start(50)
-
-    print("Do")
-    p.ChangeFrequency(523)
-    time.sleep(1)
-
-    print("Re")
-    p.ChangeFrequency(587)
-    time.sleep(1)
-   
-    print("Mi")
-    p.ChangeFrequency(659)
-    time.sleep(1)
-
-    p.stop()
-    GPIO.cleanup()
-
-doReMi()
-"""
-
 
 def play_tunes(p, freq, delay):
     p.ChangeFrequency(freq)
