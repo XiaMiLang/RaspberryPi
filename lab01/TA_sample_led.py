ecffca6f3fd775998b3fadb4452c41f65a94e765
XiaMiLang <n97071159@gs.ncku.edu.tw>
2019-10-10 21:54:17 +0800

    20191010

---
+++ lab01/TA_sample_led.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+This is a program for simple led control on RasberryPi(R)
+@author: FATESAIKOU
+@argv[1:]: ALL Pins for using
+"""
+
+import RPi.GPIO as GPIO
+import sys
+import random
+import time
+import signal
+
+shining = True
+
+def end_handler(signal, frame):
+    global shining
+    print("end of shining")
+    shining = False
+
+def initEnv():
+    GPIO.setmode(GPIO.BOARD)
+
+def initPin(pins):
+    for ledPin in pins:
+        GPIO.setup(ledPin, GPIO.OUT)
+
+def setupPin(pins):
+    for ledPin in pins:
+        GPIO.output(ledPin, True)
+
+def setoffPin(pins):
+    for ledPin in pins:
+        GPIO.output(ledPin, False)
+
+def main():
+    PINS = [int(s) for s in sys.argv[1:]]
+
+    print("Init Env")
+    initEnv()
+    initPin(PINS)
+
+    signal.signal(signal.SIGINT, end_handler)
+
+    while shining:
+        now_pins = random.sample(PINS, random.randint(1, len(PINS)))
+        print("Use Pin:", now_pins)
+    
+        print("Setup Pins")
+        setupPin(now_pins)
+        time.sleep(random.uniform(0.1, 0.7))
+    
+        print("setoff Pins")
+        setoffPin(now_pins)
+    
+    GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    main()
---
+++ lab01/TA_sample_led2.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""
+This is a program for simple led control on RasberryPi(R)
+@author: FATESAIKOU
+@argv[1:]: ALL Pins for using
+"""
+
+import RPi.GPIO as GPIO
+import sys
+import random
+import time
+import signal
+
+shining = True
+
+def end_handler(signal, frame):
+    global shining
+    print("end of shining")
+    shining = False
+
+
+def initEnv():
+    GPIO.setmode(GPIO.BOARD)
+
+
+def initPin(pins):
+    pwms = []
+    for ledPin in pins:
+        GPIO.setup(ledPin, GPIO.OUT)
+        t_pwm = GPIO.PWM(ledPin, 500)
+        t_pwm.start(100)
+        
+        pwms.append(t_pwm)
+
+    return pwms
+        
+
+def setPin(pwms, duties):
+    for i in range(len(pwms)):
+        pwms[i].ChangeDutyCycle(duties[i])
+
+
+def endShining(pwms):
+    for pwm in pwms:
+        pwm.stop()
+
+
+def main():
+    PINS = [int(s) for s in sys.argv[1:]]
+
+    print("Init Env")
+    initEnv()
+    pwms = initPin(PINS)
+
+    signal.signal(signal.SIGINT, end_handler)
+
+    while shining:
+        duties = [random.randint(0, 100) for _ in range(len(pwms))]
+        print("Use Duties:", duties)
+    
+        print("Setup Pins")
+        setPin(pwms, duties)
+        time.sleep(random.uniform(0.1, 0.7))
+
+    
+    print("Setoff Pins")
+    endShining(pwms)
+    GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    main()
