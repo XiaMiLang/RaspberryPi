e729499c4699fefa8025046fb967d1b9ba2caead
XiaMiLang <n97071159@gs.ncku.edu.tw>
2019-10-12 01:02:53 +0800

    20191012

--- lab02/lab2_buzzer_2.py
+++ lab02/lab2_buzzer_2.py
@@ -1,7 +1,7 @@
 # read pin and bee.json from command line.
-# python lab2_buzzer_2.py [pin] [freq_sequence.json]
+# python lab2_buzzer_2.py [pin] [bee.json]
 # python lab2_buzzer_2.py 12 bee.json
-
+import json
 import time
 import RPi.GPIO as GPIO
 import sys
@@ -10,36 +10,30 @@ def play_tunes(p, f, delay):
     p.ChangeFrequency(f)
     time.sleep(delay)
 
-GPIO.setmode(GPIO.BOARD)	
+GPIO.setmode(GPIO.BOARD)
 
-pin = int(sys.argv[1])		#read pin and tune from command line
-GPIO.setup(pin,GPIO.OUT)
+pin = int(sys.argv[1])      #read pin and tune from command line
+GPIO.setup(pin, GPIO.OUT)
 
-p = GPIO.PWM(pin, 50)		#p = GPIO.PWM(channel, frequency)
+p = GPIO.PWM(pin, 50)       #p = GPIO.PWM(channel, frequency)
 p.start(50)    #0 ~ 100
 delay = 0.2
-
 
 filename = sys.argv[2]
 
 with open(filename, 'r') as f:
-    data = json.loads(f)
+    data = json.load(f)
 
 freq = []
 delay = []
 
 for d in data:
     freq.append(int(d["freq"]))
-    delay.append(int(d["time"]))
+    delay.append(float(d["time"]))
 
-print freq
-print delay
-
-
-
-
-for f in freq:
-    play_tunes(p, f, delay)
+for f in range (int(len(freq))):
+    if freq[f] > 0:
+        play_tunes(p, freq[f], delay[f])
 
 p.stop()
 GPIO.cleanup()
