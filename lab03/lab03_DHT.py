8471a25829b3d96189c1b8040959439fc4958050
XiaMiLang <n97071159@gs.ncku.edu.tw>
2019-10-14 10:40:17 +0800

    20191014

--- lab03/lab03_DHT.py
+++ lab03/lab03_DHT.py
@@ -1,33 +1,28 @@
 import sys
 import Adafruit_DHT as DHT
 import time
-import os    # myCmd = 'ls > out.txt'  #os.system(myCmd)
-
+#import os    # myCmd = 'ls > out.txt'  #os.system(myCmd)
 
 def main():
-
     BCM_PIN = int(sys.argv[1])
-    TOTAL_TIME = float(sys.argv[2])    #totay time measuring
-    DELAY = float(sys.argv[3])      #delay interval
-    iter_num = int(TOTAL_TIME / DELAY)
-    condition = int(sys.argv[2])
+    condition = sys.argv[2]
+    print(condition)
+#    TOTAL_TIME = float(sys.argv[2])    #totay time measuring
+    monit_time = int(sys.argv[3])      #delay interval
+    iter_num = int(monit_time)
     print("BCM_PIN", BCM_PIN)
-    print("TOTAL_TIME", TOTAL_TIME)
-    print("DELAY", DELAY)
-    print("iter_num", iter_num)
-    
-    
-    
-#    while True: 
-    for i in range(iter_num):
+#    print("TOTAL_TIME", TOTAL_TIME)
+    print("monit_time", monit_time)
+#    print("iter_num", iter_num)
+#    while True:
+    for i in range(monit_time):
         h, t = DHT.read_retry(11, BCM_PIN)    #h, t=DHT.read_retry(DHT_version, BCM_PIN)
-        print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(t, h))
-        if (condition == 1) and (t>1):
-            print("t>1")
-        if (condition == 2) and (t>11) and (h>43):
-            print("t>11 & h>0.43")
-            
-        time.sleep(DELAY)
+        condition = condition.replace("t", str(t)).replace("h", str(h))
+
+        if (eval(condition)):
+            print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(t, h))
+#        print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(t, h))
+        time.sleep(1)
     
 if __name__=="__main__":
     main()
