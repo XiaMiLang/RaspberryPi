f179db77e8d12191931500de57075c790d4a1b41
XiaMiLang <n97071159@gs.ncku.edu.tw>
2019-10-24 16:52:40 +0800

    20191024

--- lab04/ir.py
+++ lab04/ir.py
@@ -22,85 +22,85 @@ def setoffPin(ledPin):
         
         
 def compairSignal(s1,s2,rang):
-	min_len = min(len(s1),len(s2))
-	for i in range(min_len):
-		print(str(sc1)+"="+str(sc2))
-		if sc1!=sc2:
-			return False 
-		
-	return True
-	
+    min_len = min(len(s1),len(s2))
+    for i in range(min_len):
+        print(str(s1)+"="+str(s2))
+        if s1!=s2:
+            return False 
+        
+    return True
+    
 def decodeSingal(s,signal_map,rang):
-	for name in signal_map.keys():
-		print(name)
-		if compairSignal(s,signal_map[name],rang):
-			return name
-	return None
-	
+    for name in signal_map.keys():
+        print(name)
+        if compairSignal(s,signal_map[name],rang):
+            return name
+    return None
+    
 def getSignal(pin):
-	start, stop = 0, 0
+    start, stop = 0, 0
 
-	signals = []
-	while True:
-		while GPIO.input(pin) == 0:
-			None
+    signals = []
+    while True:
+        while GPIO.input(pin) == 0:
+            None
 
-		start = time.time()
+        start = time.time()
 
-		while GPIO.input(pin) == 1:
-			stop = time.time()
+        while GPIO.input(pin) == 1:
+            stop = time.time()
 
-			duringUp = (stop - start)*1000 # using ms
-			if duringUp > 100  and len(signals) > 0:
-				return signals[1:]
+            duringUp = (stop - start)*1000 # using ms
+            if duringUp > 100  and len(signals) > 0:
+                return signals[1:]
 
-		signals.append(duringUp)
-		
+        signals.append(duringUp)
+        
 def initEnv(IR_pin,LED_pin):
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setwarnings(False)
-	GPIO.setup(LED_pin, GPIO.OUT)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+    GPIO.setup(LED_pin, GPIO.OUT)
 
-	GPIO.setup(IR_pin, GPIO.IN)
+    GPIO.setup(IR_pin, GPIO.IN)
 
 def endEnv():
-	GPIO.cleanup()
-	
+    GPIO.cleanup()
+    
 def changecode(s):
-	res =''
-	if s > 30 and s < 50: # frame space/stop(30~50ms) 
-		res ='>'
-	elif s > 2 and s < 10: # leading pulse burst (4.5ms) 
-		res ='<'
-	elif s > 1.0 and s < 2: # 1 (1.7ms)
-		res ='-'
-	elif s > 0.1 and s < 1: # 0 (0.56ms)
-		res ='|'
-	else:
-		res ='.'
-		
-	return res
-	
+    res =''
+    if s > 30 and s < 50: # frame space/stop(30~50ms) 
+        res ='>'
+    elif s > 2 and s < 10: # leading pulse burst (4.5ms) 
+        res ='<'
+    elif s > 1.0 and s < 2: # 1 (1.7ms)
+        res ='-'
+    elif s > 0.1 and s < 1: # 0 (0.56ms)
+        res ='|'
+    else:
+        res ='.'
+        
+    return res
+    
 def main():
-	IR_PIN = int(sys.argv[1])
-	LED_PIN = int(sys.argv[2])
-	FILEPATH = sys.argv[3]
-	with open(FILEPATH,'r') as f:
-		key_map = json.load(f)
-	initEnv(IR_PIN,LED_PIN)
-	
-	while True:
-		print("please press key")
-		
-		s = getSignal(IR_PIN)
-		usekey = (decodeSingal(s,key_map,0.05))
-		print("Your press %s"%usekey)
-		if usekey == "1" :
-			setupPin(LED_PIN)
-		elif usekey == "0":
-			setoffPin(LED_PIN)
-	endEnv()
+    IR_PIN = int(sys.argv[1])
+    LED_PIN = int(sys.argv[2])
+    FILEPATH = sys.argv[3]
+    with open(FILEPATH,'r') as f:
+        key_map = json.load(f)
+    initEnv(IR_PIN,LED_PIN)
+    
+    while True:
+        print("please press key")
+        
+        s = getSignal(IR_PIN)
+        usekey = (decodeSingal(s,key_map,0.05))
+        print("Your press %s"%usekey)
+        if usekey == "1" :
+            setupPin(LED_PIN)
+        elif usekey == "0":
+            setoffPin(LED_PIN)
+    endEnv()
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- lab04/irrecord.py
+++ lab04/irrecord.py
@@ -1,79 +1,60 @@
-#!/usr/bin/env python3
-"""
-This is a program for recording your IR-device signal.
-
-@author: FATESAIKOU
-@argv[1]: data input pin(BOARD)
-@argv[2]: the output filename for key_map
-"""
-
 import RPi.GPIO as GPIO
 import time
 import sys
 import json
 
 def initEnv(pin):
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setwarnings(False)
-
-	GPIO.setup(pin, GPIO.IN)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+    GPIO.setup(pin, GPIO.IN)
 
 def endEnv():
-	GPIO.cleanup()
-
+    GPIO.cleanup()
 
 def getSignal(pin):
-	start, stop = 0, 0
-
-	signals = []
-	while True:
-		while GPIO.input(pin) == 0:
-			None
-
-		start = time.time()
-
-		while GPIO.input(pin) == 1:
-			stop = time.time()
-
-			duringUp = (stop - start)*1000 # using ms
-			if duringUp > 100  and len(signals) > 0:
-				return signals[1:]
-
-		signals.append(duringUp)
-
+    start, stop = 0, 0
+    signals = []
+    while True:
+        while GPIO.input(pin) == 0:
+            None
+        start = time.time()
+        while GPIO.input(pin) == 1:
+            stop = time.time()
+            duringUp = (stop - start)*1000 # using ms
+            if duringUp > 100  and len(signals) > 0:
+                return signals[1:]
+        signals.append(duringUp)
 def main():
-	PIN = int(sys.argv[1])
-	OUT_FILE = sys.argv[2]
+    PIN = int(sys.argv[1])
+    OUT_FILE = sys.argv[2]
+    initEnv(PIN)
+    keys = {}
+    while True:
+        key_name = input('Please input key name(exit for terminating this program):')
+        if key_name == 'exit':
+            break
 
-	initEnv(PIN)
+        keys[key_name] = getSignal(PIN)
 
-	keys = {}
-	while True:
-		key_name = input('Please input key name(exit for terminating this program):')
-		if key_name == 'exit':
-			break
+        for pulse in keys[key_name]:
+            if pulse > 30 and pulse < 50: # frame space/stop(30~50ms) 
+                print('>', end='')
+                break
+            elif pulse > 2 and pulse < 10: # leading pulse burst (4.5ms) 
+                print('<', end='')
+            elif pulse > 1.0 and pulse < 2: # 1 (1.7ms)
+                print('-', end='')
+            elif pulse > 0.1 and pulse < 1: # 0 (0.56ms)
+                print('|', end='')
+            else:
+                print('.', end='') # other
+        print('')
 
-		keys[key_name] = getSignal(PIN)
+    endEnv()
 
-		for pulse in keys[key_name]:
-			if pulse > 30 and pulse < 50: # frame space/stop(30~50ms) 
-				print('>', end='')
-				break
-			elif pulse > 2 and pulse < 10: # leading pulse burst (4.5ms) 
-				print('<', end='')
-			elif pulse > 1.0 and pulse < 2: # 1 (1.7ms)
-				print('-', end='')
-			elif pulse > 0.1 and pulse < 1: # 0 (0.56ms)
-				print('|', end='')
-			else:
-				print('.', end='') # other
-		print('')
-
-	endEnv()
-
-	src = open(OUT_FILE, 'w')
-	src.write(json.dumps(keys))
-	src.close()
+    src = open(OUT_FILE, 'w')
+    src.write(json.dumps(keys))
+    src.close()
 
 if __name__ == "__main__":
-	main()
+    main()
