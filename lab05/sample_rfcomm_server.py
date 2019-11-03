5a725fba9dd857a01b5a51101edf7c4c48f8217c
XiaMiLang <n97071159@gs.ncku.edu.tw>
2019-11-03 23:36:50 +0800

    20191103

--- lab05/sample_rfcomm_server.py
+++ lab05/sample_rfcomm_server.py
@@ -8,6 +8,28 @@ please check your BlueTooth connection before you start your testing.
 import signal
 import sys
 from bluetooth import *
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+RED_LED_PIN = 36
+GREEN_LED_PIN = 38
+BLUE_LED_PIN = 40
+PWM_FREQ = 200
+GPIO.setup(RED_LED_PIN, GPIO.OUT)
+GPIO.setup(GREEN_LED_PIN, GPIO.OUT)
+GPIO.setup(BLUE_LED_PIN, GPIO.OUT)
+red_pwm = GPIO.PWM(RED_LED_PIN, PWM_FREQ)
+red_pwm.start(0)
+blue_pwm = GPIO.PWM(BLUE_LED_PIN, PWM_FREQ)
+blue_pwm.start(0)
+green_pwm = GPIO.PWM(GREEN_LED_PIN, PWM_FREQ)
+green_pwm.start(0)
+
+def setColor(r=0, g=0, b=0):
+    red_pwm.ChangeDutyCycle(r)
+    blue_pwm.ChangeDutyCycle(g)
+    green_pwm.ChangeDutyCycle(b)
 
 # Service ending handler
 def end_service(signal, frame):
@@ -20,18 +42,32 @@ def end_service(signal, frame):
 def handler(sock, info):
     global service_on
     print("[INFO] Accepted connection from", info)
-
     try:
         while service_on:
-            data = sock.recv(1024)
-            if len(data) == 0: break
-
+            data = str(sock.recv(1024), encoding = "utf-8")
+            if len(data) == 0:
+                break
+            request = data.split()
+            print(request)
             print("[RECV] %s" % data)
-            sock.send('foo ' + data.decode('ascii'))
-            print("[SEND] >> foo " + data.decode('ascii'))
+            if request[0] == "set":
+                print(request[1]+":"+request[2]+":"+request[3])
+                # duties = [int(request[1]), int(request[2]), int(request[3])]
+                # print(duties)
+                values = [int(request[1]), int(request[2]), int(request[3])]
+                setColor(int(request[1]), int(request[2]), int(request[3]))
+                time.sleep(0.5)
+                sock.send('led changed')
+            elif request[0]=="get" and request[1]=="values":
+                msg = "[send] >> " + str(values[1]) + ":" + str(values[2]) + ":"+ str(values[3])
+                sock.send(msg)
+                print("[send]" + msg)
+            else :
+                sock.send('invalid input')
+            # sock.send('foo ' + data.decode('ascii'))
+            # print("[SEND] >> foo " + data.decode('ascii'))
     except IOError:
         pass
-        
 # Env init
 #UUID = '94f39d29-7d6d-437d-973b-fba39e49d4ee'
 UUID = '00001112-0000-1000-8000-00805f9b34fb'
